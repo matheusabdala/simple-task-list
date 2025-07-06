b561d3c1924cd2b28ce18a7640e5c8b311b68aa8
Matheus Abdala <theus0702@gmail.com>
2025-07-06 01:31:43 -0300

    a commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AddTask } from "./components/AddTask";
 import { Tasks } from "./components/Tasks";
 
@@ -5,31 +6,41 @@ export function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação para se tornar um grande dev",
+      title: "Fazer Café",
+      description: "descrição da tarefa atual",
       isCompleted: false,
     },
     {
       id: 2,
       title: "Estudar programação",
-      description: "Estudar programação para se tornar um grande dev",
+      description: "descrição da tarefa atual",
       isCompleted: false,
     },
     {
       id: 3,
-      title: "Estudar programação",
-      description: "Estudar programação para se tornar um grande dev",
+      title: "Berber água",
+      description: "descrição da tarefa atual",
       isCompleted: false,
     },
   ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
-        <Tasks />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,3 +1,23 @@
-export function Tasks() {
-  return <h1>tasks</h1>;
+import { ChevronRightIcon } from "lucide-react";
+
+export function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`${
+              task.isCompleted && "line-through"
+            } bg-slate-400 text-left w-full text-white p-2 rounded-md cursor-pointer`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white cursor-pointer">
+            <ChevronRightIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
